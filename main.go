592e21b7e94bf238c2d43c97f8a896f54629c09d
jkisiel <kisieljaku@gmail.com>
2022-04-30 23:59:45 +0200

    Added paths

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"sync"
+	"time"
 )
 
 type HttpHandler struct{}
@@ -13,13 +14,22 @@ func (h HttpHandler) ServeHTTP(res http.ResponseWriter, r *http.Request) {
 	io.WriteString(res, "Hello, world!\n")
 }
 
+func ServeRoot(res http.ResponseWriter, r *http.Request) {
+	io.WriteString(res, "Hello, "+r.Host)
+}
+func ServeTime(res http.ResponseWriter, r *http.Request) {
+	io.WriteString(res, "Hello "+r.Host+" It's "+time.Now().Format("2006-01-02T15:04"))
+}
+
 func main() {
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		fmt.Println("Starting Http server")
-		http.ListenAndServe(":9000", HttpHandler{})
+		http.HandleFunc("/", ServeRoot)
+		http.HandleFunc("/time/", ServeTime)
+		http.ListenAndServe(":9000", nil)
 	}()
 	wg.Wait()
 	fmt.Println("Server is closed")
